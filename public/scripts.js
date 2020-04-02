843473acccfd64004b48dae5cab0766b7d7787cb
unknown <leonardo.1117@hotmail.com>
2020-04-02 07:08:18 -0300

    adicionada funcao que nao carrega o topico na tela se ele estiver vazio

--- public/scripts.js
+++ public/scripts.js
@@ -13,54 +13,45 @@ for (let receita of recepies) {
   })
 }
 
-
 const blocoIngredientes = document.querySelector('.block__ingredients')
 const blocoPreparacao = document.querySelector('.block__preparation')
 const blocoInformacoes = document.querySelector('.block__information')
+const titleInformacoes = document.querySelector('.title__information')
 
 
-if(blocoInformacoes==''){
-  blocoInformacoes.style.display = none;
-}
 
-
+//função de mostrar/esconder informações com os botões na frente dos tópicos da página
 function showInformation(informationToShow, buttonClicked) {
+
+
 
   if (informationToShow.style.display == 'none') {
     buttonClicked.addEventListener("click", function () {
       informationToShow.style.display = 'flex'
       buttonClicked.innerHTML = 'ESCONDER'
+      console.log('clicked')
     })
   } else {
     buttonClicked.addEventListener("click", function () {
       informationToShow.style.display = 'none'
       buttonClicked.innerHTML = 'MOSTRAR'
+      console.log('clicked')
+
     })
   }
 }
 
 
 
+const paragrafoInformacao = document.getElementById('infoP')
+
+window.onload = function verifyInfo() {
 
 
 
-
-
-
-//"FUNCIONANDO" com dois clicks e algo está errado com os parâmetros, os botões estão mostrando/escondendo outras divs
-// function showInformation(informationToShow){
-
-//   if(informationToShow.style.display == 'none'){
-//     btnClose.addEventListener("click", function () {
-//       informationToShow.style.display = 'initial'
-//       btnClose.innerHTML = 'Esconder'
-//     })
-//   }else{
-//     btnClose.addEventListener("click", function () {
-//       informationToShow.style.display = 'none'
-//       btnClose.innerHTML = 'Mostrar'
-//     })
-//   }
-// }
+  if (paragrafoInformacao.innerHTML == "") {
+    titleInformacoes.style.display = 'none';
+  }
+}
 
 
