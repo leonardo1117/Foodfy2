c22144be4d4aa99af5991768565adbeb1db848b3
unknown <leonardo.1117@hotmail.com>
2020-04-03 04:50:21 -0300

    adicionado efeitos visuais ao painel de receitas e header

--- public/scripts.js
+++ public/scripts.js
@@ -23,35 +23,28 @@ const titleInformacoes = document.querySelector('.title__information')
 //função de mostrar/esconder informações com os botões na frente dos tópicos da página
 function showInformation(informationToShow, buttonClicked) {
 
+    if (informationToShow.style.display == 'none') {
+      buttonClicked.addEventListener("click", function () {
+        informationToShow.style.display = 'flex'
+        buttonClicked.innerHTML = 'ESCONDER'
+        console.log('clicked')
+      })
+    } else {
+      buttonClicked.addEventListener("click", function () {
+        informationToShow.style.display = 'none'
+        buttonClicked.innerHTML = 'MOSTRAR'
+        console.log('clicked')
 
-
-  if (informationToShow.style.display == 'none') {
-    buttonClicked.addEventListener("click", function () {
-      informationToShow.style.display = 'flex'
-      buttonClicked.innerHTML = 'ESCONDER'
-      console.log('clicked')
-    })
-  } else {
-    buttonClicked.addEventListener("click", function () {
-      informationToShow.style.display = 'none'
-      buttonClicked.innerHTML = 'MOSTRAR'
-      console.log('clicked')
-
-    })
+      })
+    }
   }
-}
-
-
 
 const paragrafoInformacao = document.getElementById('infoP')
 
 window.onload = function verifyInfo() {
 
-
-
   if (paragrafoInformacao.innerHTML == "") {
     titleInformacoes.style.display = 'none';
   }
 }
-
 
