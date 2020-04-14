0e9398e9402c6637bb2245cdb35a2bace61a01a6
unknown <leonardo.1117@hotmail.com>
2020-04-13 21:00:26 -0300

    criado padrão de layout dentro do arquivo layout.njk e importado nos arquivos que utilizam o mesmo layout

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const routes = require('./routes')
 
 const server = express()
 
-server.set("view engine", "njk")
 
 server.use(express.static('public'))
 server.use(routes)
+
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
   express: server,
