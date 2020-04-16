d352187fa3f0c996c5536c2e19b6c7e0373856ea
unknown <leonardo.1117@hotmail.com>
2020-04-15 22:23:16 -0300

    criado lógica para implementação de novo ingrediente/passo ajustada e agora os novos elementos são criados abaixo dos anteriores

--- public/scripts.js
+++ public/scripts.js
@@ -18,8 +18,8 @@ const blocoInformacoes = document.querySelector('.block__information')
 const titleInformacoes = document.querySelector('.title__information')
 
 //função de mostrar/esconder informações com os botões na frente dos tópicos da página
-function showInformation(informationToShow, buttonClicked){
-  informationToShow.style.display != "flex" ? (informationToShow.style.display = "flex", buttonClicked.innerHTML = "ESCONDER") : (informationToShow.style.display = "none", buttonClicked.innerHTML = "MOSTRAR") 
+function showInformation(informationToShow, buttonClicked) {
+  informationToShow.style.display != "flex" ? (informationToShow.style.display = "flex", buttonClicked.innerHTML = "ESCONDER") : (informationToShow.style.display = "none", buttonClicked.innerHTML = "MOSTRAR")
 }
 
 const paragrafoInformacao = document.getElementById('infoP')
@@ -35,12 +35,24 @@ window.onload = function verifyInfo() {
 // ADICIONAR NOVO CAMPO COM BOTÃO 
 
 const btnAddIngredient = document.getElementById('add-ingredient')
-const btnAddStep = document.getElementById('add-step')
 const ingredientsForm = document.querySelector('.ingredients')
 
-btnAddIngredient.addEventListener("click", function(event){
-  let newField = document.createElement('input')
-  newField.name = "ingredients[]"
-  newField.placeholder = "Novo ingrediente"
-  ingredientsForm.appendChild(newField)
+btnAddIngredient.addEventListener("click", function (event) {
+  let newIngredient = document.createElement('input')
+  newIngredient.name = "ingredients[]"
+  newIngredient.placeholder = "Novo ingrediente"
+  ingredientsForm.appendChild(newIngredient)
+  this.parentElement.insertBefore(newIngredient, this.parentElement.children[2])
+
+})
+
+const btnAddStep = document.getElementById('add-step')
+const preparationForm = document.querySelector('.preparation')
+
+btnAddStep.addEventListener("click", function () {
+  let newStep = document.createElement('input')
+  newStep.name = "preparation[]"
+  newStep.placeholder = "Novo passo"
+  preparationForm.appendChild(newStep)
+  this.parentElement.insertBefore(newStep, this.parentElement.children[2])
 })
