d898fd4a6147a5ac7fc6b0d99537db644b9e143d
unknown <leonardo.1117@hotmail.com>
2020-05-22 19:30:40 -0300

    criado função de paginação

--- public/scripts.js
+++ public/scripts.js
@@ -79,3 +79,31 @@ document
     addInput(steps, preparation)
   })
 
+
+
+function paginate(selectedPage, totalPages) {
+  let pages = [],
+    oldPage
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if (firstAndLastPage || pagesAfterSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...")
+      }
+      if(oldPage && currentPage - oldPage == 2){
+        pages.push(oldPage + 1)
+      }
+      pages.push(currentPage)
+
+      oldPage = currentPage
+    }
+  }
+
+  return pages
+  
+}
+
