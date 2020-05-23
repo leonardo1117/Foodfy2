422f648bca8f2af504a9660490380150819262e4
unknown <leonardo.1117@hotmail.com>
2020-05-23 18:24:37 -0300

    continuação de desenvolvimento na lógica de paginação na index dos usuários

--- public/scripts.js
+++ public/scripts.js
@@ -94,7 +94,7 @@ function paginate(selectedPage, totalPages) {
       if (oldPage && currentPage - oldPage > 2) {
         pages.push("...")
       }
-      if(oldPage && currentPage - oldPage == 2){
+      if (oldPage && currentPage - oldPage == 2) {
         pages.push(oldPage + 1)
       }
       pages.push(currentPage)
@@ -104,6 +104,34 @@ function paginate(selectedPage, totalPages) {
   }
 
   return pages
-  
+
 }
 
+
+function createPagination(pagination) {
+  const page = +pagination.dataset.page
+  const total = +pagination.dataset.total
+  const filter = +pagination.dataset.filter
+  const pages = paginate(page, total)
+
+  let elements = ""
+
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`
+    } else {
+      if (filter) {
+        elements += `<a href=?page${page}&filter=${filter}>${page}</a>`
+      } else {
+        elements += `<a href="?page=${page}>${page}</a>`
+      }
+    }
+  }
+  pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector('.pagination')
+
+if (pagination) {
+  createPagination(pagination)
+}
