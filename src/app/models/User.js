60eb8308dff2cc191754c320fd43cdee0787a389
unknown <leonardo.1117@hotmail.com>
2020-05-24 20:27:47 -0300

    criado query para exibição de receitas na index do usuário com lógica para filtro e paginação das receitas

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -10,5 +10,47 @@ module.exports = {
 
       callback(results.rows)
     })
+  },
+  pagination(params) {
+    const { filter, limit, offset, callback } = params
+
+    let query = "",
+      filterQuery = "",
+      totalQuery = `SELECT recipes.*, chefs.name AS chef_name 
+    FROM recipes 
+    LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+    `
+
+    if (filter) {
+
+      filterQuery = `
+        WHERE recipes.title ILIKE '%${filter}%'
+      `
+
+      totalQuery = `
+        SELECT recipes.*, chefs.name AS chef_name 
+        FROM recipes 
+        ${filterQuery}
+        LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+      `
+
+      query = `
+        SELECT recipes.*, ${totalQuery}
+        FROM recipes
+        ${filterQuery}
+        LIMIT $1 OFFSET $2
+      `
+
+      db.query(query, [limit, offset], function (err, results) {
+        if (err) throw `Database error ${err}`
+
+        callback(results.rows)
+      })
+    }
   }
 }
+
+
+
+
+
