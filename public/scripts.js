55fed574d06e15a0213ea65ed6c04972c0ede355
unknown <leonardo.1117@hotmail.com>
2020-09-27 22:47:41 -0300

    add logic for adding/removing files from fileList(event.target)//add style to photo preview on create recipe page

--- public/scripts.js
+++ public/scripts.js
@@ -146,35 +146,80 @@ document
 // PHOTOS UPLOAD
 
 const PhotosUpload = {
+  input: "",
+  files: [],
+  preview: document.querySelector('#photos-preview'),
   uploadLimit: 5,
   handleFileInput(event) {
     const { files: fileList } = event.target
-    const { uploadLimit } = PhotosUpload
 
-    if(fileList.length > uploadLimit) {
-      alert(`Por favor, envie no máximo ${uploadLimit} fotos da receita!`)
-      event.preventDefault()
-      return
-    }
-  
+    PhotosUpload.input = event.target
+
+    if (PhotosUpload.hasLimit(event)) return
+
     Array.from(fileList).forEach(file => {
+
+      PhotosUpload.files.push(file)
+
       const reader = new FileReader()
 
       reader.onload = () => {
         const image = new Image()
         image.src = String(reader.result)
 
-        const div = document.createElement('div')
-        div.classList.add('photo')
+        const div = PhotosUpload.getContainer(image)
 
-        div.onclick = () => alert('excluir imagem')
+        PhotosUpload.preview.appendChild(div)
 
-        div.appendChild(image)
-
-        document.querySelector('#photos-preview').appendChild(div)
       }
       reader.readAsDataURL(file)
     })
 
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+    div.classList.add('photo')
+
+    div.onclick = PhotosUpload.removePhoto
+
+    div.appendChild(image)
+
+    div.appendChild(PhotosUpload.getRemoveButton())
+
+    return div
+  },
+  hasLimit(event) {
+    const { uploadLimit, input: fileList } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Por favor, envie no máximo ${uploadLimit} fotos da receita!`)
+      event.preventDefault()
+      return true
+    }
+    return false
+  },
+  getAllFiles() {
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+    PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+    return dataTransfer.files
+  },
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+
+    button.innerHTML = 'close'
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(photoDiv)
+
+    PhotosUpload.files.splice(index, 1)
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+    photoDiv.remove()
   }
 }
